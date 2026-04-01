f2c0c688da355803eb4d4a888fe9943e8c2749f7
Sanjana-Hossain <cjh8az@virginia.edu>
2026-04-01 16:54:42 -0400

    -reading in data

---
+++ src/ertimes/burden_analysis.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np 
+
+import pandas as pd
+
+# Replace 'your_file.xlsx' with the actual file path
+df = pd.read_excel(r"C:\Users\sanja\OneDrive\Desktop\stat3250\ertimes\data\emergency-department-volume-and-capacity-2021-2023.xlsx")
+
+# See the first few rows to check your data
+print(df.head())
