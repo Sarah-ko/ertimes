2bc2a8d5a5e342fab05a15824c5c7383027567e0
Jess Zhou <zhoujessica23@gmail.com>
2026-04-06 16:30:24 -0400

    Add map viz

--- src/ertimes/map_viz.py
+++ src/ertimes/map_viz.py
@@ -1,39 +1,53 @@
 import pandas as pd
-from src.ertimes.map_viz import create_ed_map
+import plotly.express as px
 
-data = {
-    "ed_id": [1,2,3,4],
-    "ed_name": ["UVA Hospital", "Sentara Martha Jefferson", "Augusta Health", "Carilion Roanoke"],
-    "latitude": [38.03, 38.03, 38.08, 37.27],
-    "longitude": [-78.50, -78.48, -78.99, -79.94],
-    "year": [2021, 2022, 2022, 2023],
-    "total_ed_visits": [70000, 35000, 25000, 60000],
-    "county": ["Charlottesville", "Charlottesville", "Augusta", "Roanoke"],
-    "primary_care_shortage": ["Yes", "No", "Yes", "No"],
-    "mental_health_shortage": ["Yes", "Yes", "No", "Yes"]
-}
+def create_ed_map(df, year):
+    """
+    Create an interactive map of EDs for a given year.
+    
+    Parameters:
+        df (pd.DataFrame): DataFrame with ED info
+        year (int): Year to filter the dataset
+    Returns:
+        fig (plotly.graph_objects.Figure): Interactive map figure
+    """
+    # Filter for the selected year
+    df_year = df[df["year"] == year]
+    
+    # Map primary care shortage to colors
+    color_map = {"Yes": "red", "No": "green"}
+    
+    # Create the scatter mapbox
+    fig = px.scatter_mapbox(
+        df_year,
+        lat="latitude",
+        lon="longitude",
+        size="total_ed_visits",
+        color="primary_care_shortage",
+        color_discrete_map=color_map,
+        hover_name="ed_name",
+        hover_data={
+            "total_ed_visits": True,
+            "county": True,
+            "primary_care_shortage": True,
+            "mental_health_shortage": True,
+            "latitude": False,
+            "longitude": False
+        },
+        zoom=7,
+        height=600
+    )
+    
+    fig.update_layout(
+        mapbox_style="open-street-map",
+        title=f"Emergency Department Visits in {year}",
+        margin={"r":0,"t":40,"l":0,"b":0}
+    )
+    
+    return fig
 
-df = pd.DataFrame(data)
 
-df.to_csv("ed_volume_dataset.csv", index=False)
 
-# Testing this function on my mini dataset:
 
-# df = pd.read_csv("ed_volume_dataset.csv")
-# create_ed_map(df, 2022).show()
-# the above code would work if I ran it directly in my map_viz file, but because
-# this is a test file, I have to use other packages to be able to have the map
-# open in a browser tab
 
-import webbrowser
-import os
 
-# Create the map
-fig = create_ed_map(df, 2022)
-
-# Save to a temporary file
-filename = "temp_map.html"
-fig.write_html(filename)
-
-# Open the file in a new tab
-webbrowser.open('file://' + os.path.realpath(filename))
