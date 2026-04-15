61c52668da5224cb8714ce9170a23c6d8566d6d1
LinneaSaunders <dzd5zg@virginia.edu>
2026-04-15 14:46:52 -0400

    Add files via upload
    Added file that reads in demographic data

---
+++ demodata.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from democlean import clean_data
+
+DATA_URLS = {
+    "calidemodata": "https://docs.google.com/spreadsheets/d/e/2PACX-1vThWsHWEJm2Kr_HFyesdrkKOcQIkoNqQXBI_wHjeTbrcbXvr4ak3IaDZJkHTzfalTdYbIf0T0mGIMbz/pub?output=csv"
+}
+
+
+def download_data(dataset: str) -> pd.DataFrame:
+    dataset_lower = dataset.lower()
+
+    if dataset_lower not in DATA_URLS:
+        raise ValueError(f"Dataset '{dataset}' is not supported")
+
+    url = DATA_URLS[dataset_lower]
+    df = pd.read_csv(url)
+    df = clean_data(df)
+
+    return df
+
+
+def load_data(filepath: str) -> pd.DataFrame:
+    df = pd.read_csv(filepath)
+    df = clean_data(df)
+    return df
